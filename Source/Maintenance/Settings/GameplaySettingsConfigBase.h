321c1fd6f4ad2338cc4325417b53a4b8e3ab82a9
CreativeMetalCat <catguy228@gmail.com>
2020-09-23 11:06:11 +1000

    added base config

---
+++ Source/Maintenance/Settings/GameplaySettingsConfigBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CustomConfigObject.h"
+#include "GameplaySettingsConfigBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config = Settings)
+class MAINTENANCE_API UGameplaySettingsConfigBase : public UCustomConfigObject
+{
+	GENERATED_BODY()
+	
+};
